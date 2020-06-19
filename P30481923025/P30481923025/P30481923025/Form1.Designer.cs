723e9a5c23af818830667ad1ee30ce6974d550fd
Vinícius Souza <39967235+vinifsouza@users.noreply.github.com>
2020-06-19 20:39:39 -0300

    update

---
+++ P30481923025/P30481923025/P30481923025/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace P30481923025 {
+    partial class Form1 {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 16;
+            this.listBox.Location = new System.Drawing.Point(492, 13);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(296, 420);
+            this.listBox.TabIndex = 0;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(189, 135);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(234, 155);
+            this.btnVerificar.TabIndex = 1;
+            this.btnVerificar.Text = "Inserir entradas";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.listBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ P30481923025/P30481923025/P30481923025/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
+
+
+namespace P30481923025 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e) {
+            double[,] vendas = new double[5, 4];
+            string totalSemana = "";
+            double numSemana = 0;
+            double somaGeral = 0;
+            bool validar = true;
+            int sair = 0;
+            
+            for(var i = 0; i < 5; i++) {
+                double soma = 0;
+                validar = true;
+                sair = 0;
+
+                for (var j = 0; j < 4; j++) {
+
+                    do {
+                        totalSemana = Interaction.InputBox("Digite o total da semana: " + (j + 1) +
+                            " Mês " + (i + 1));
+
+                        if(totalSemana == "") {
+                            MessageBox.Show("Entrada vazia!");
+                            sair = 1;
+                            break;
+                        }else if (!double.TryParse(totalSemana, out numSemana)) {
+                            MessageBox.Show("Digite um valor válido!");
+                            validar = false;
+                        } else{
+                            validar = true;
+                        }
+                    } while (validar == false);
+
+                    if (sair == 1) {
+                        break;
+                    } else { 
+                    listBox.Items.Add("Total do mês: " + (i + 1) + " Semana: " + (j + 1) + " R$ " +
+                                numSemana.ToString("N2"));
+                    soma += numSemana;
+                    }
+                }
+
+                if(sair == 1) {
+                    break;
+                } else { 
+                somaGeral += soma;
+                listBox.Items.Add(">> Total Mês: R$ " + soma.ToString("N2"));
+                listBox.Items.Add("__________");
+                }
+            }
+            listBox.Items.Add(">>Total Geral: R$ " + somaGeral.ToString("N2"));
+        }
+    }
+}
